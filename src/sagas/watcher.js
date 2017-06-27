0ca87ac05d120228980627e01684e0931b5a1419
sirius44 <mobiletop527@gmail.com>
2017-06-27 15:28:54 +0800

    first commit

---
+++ src/sagas/watcher.js
@@ -0,0 +1,9 @@
+import { takeLatest } from 'redux-saga/effects';
+import { searchMediaSaga } from './mediaSaga';
+import * as types from '../constants/actionTypes';
+
+// Watches for SEARCH_MEDIA_REQUEST action type asynchronously
+export default function* watchSearchMedia() {
+	console.log('[watchSearchMedia]')
+  	yield takeLatest(types.SEARCH_MEDIA_REQUEST, searchMediaSaga);
+}
